b1809405aa21e8eefb1e134f35c3461430e3b34a
OguzKagankrks <okagankrks@gmail.com>
2025-08-28 08:22:19 +0300

    4. commit: test ayarları ve küçük düzenleme

--- cypress/e2e/register-test-project.cy.js
+++ cypress/e2e/register-test-project.cy.js
@@ -1,35 +1,66 @@
+
+beforeEach(() => {
+  cy.visit('http://localhost:5173/')
+})
+
 describe('Register Page', () => {
   
   describe('Error Messages', () => {
-    it('Name input throws error for 2 chars', () => {
-      cy.visit('http://localhost:5173/')
-      cy.get('[data-cy="isim-input"]').type('ab')
-      cy.contains('İsim en az 3 karakter uzunluğunda olmalıdır.').should('be.visible')
-    })
+  it('Name input throws error for 2 chars', () => {
 
-    it('Surname input throws error for 2 chars', () => {
-      cy.visit('http://localhost:5173/')
-      cy.get('[data-cy="soyisim-input"]').type('ab')
-      cy.contains('Soyisim en az 3 karakter uzunluğunda olmalıdır.').should('be.visible')
-    })
+    cy.get('[data-cy="isim-input"]').type('ab')
 
-    it('The error displayed when a valid email address is not entered.', () => {
-      cy.visit('http://localhost:5173/')
-      cy.get('[data-cy="email-input"]').type('emre@wit.')
-      cy.contains('Geçerli bir email adresi giriniz.').should('be.visible')
-    })
+    cy.contains('İsim en az 3 karakter uzunluğunda olmalıdır.').should('be.visible')
+  })
+   it('Surname input throws error for 2 chars', () => {
 
-    it('The error displayed when a valid password is not entered.', () => {
-      cy.visit('http://localhost:5173/')
-      cy.get('[data-cy="password-input"]').type('test1234!')
-      cy.contains('Şifre en az 8 karakter uzunluğunda, en az bir büyük harf, bir küçük harf, bir rakam ve bir özel karakter içermelidir.').should('be.visible')
-    })
+    cy.get('[data-cy="soyisim-input"]').type('ab')
+   
+    cy.contains('Soyisim en az 3 karakter uzunluğunda olmalıdır.').should('be.visible')
+  })
+   it('The error displayed when a valid email address is not entered.', () => {
 
-    it('Button is disabled for unvalidated inputs', () => {
-      cy.visit('http://localhost:5173/')
-      cy.get('[data-cy="password-input"]').type('test1234!')
-      cy.get('[data-cy="submit-button"]').should('be.disabled')
-    })
+    cy.get('[data-cy="email-input"]').type('emre@wit.')
+   
+    cy.contains('Geçerli bir email adresi giriniz.').should('be.visible')
+  })
+     it('The error displayed when a valid password is not entered.', () => {
+
+    cy.get('[data-cy="password-input"]').type('test1234!')
+   
+    cy.contains('Şifre en az 8 karakter uzunluğunda, en az bir büyük harf, bir küçük harf, bir rakam ve bir özel karakter içermelidir.').should('be.visible')
+  })
+   it('Button is disabled for unvalidated inputs', () => {
+
+    cy.get('[data-cy="password-input"]').type('test1234!')
+
+    cy.get('[data-cy="submit-button"]').should('be.disabled')
+   
   })
  })
+  describe('Successful Registration', () => {
+   it('Form can be submitted when all inputs are valid', () => {
 
+    cy.get('[data-cy="isim-input"]').type('Emre')
+    cy.get('[data-cy="soyisim-input"]').type('Kaya')
+    cy.get('[data-cy="email-input"]').type('emrekaya@gmail.com')
+    cy.get('[data-cy="password-input"]').type('Test1234!')
+    cy.get('[data-cy="submit-button"]').should('not.be.disabled')
+  
+   })
+      it('Submits form on validated inputs', () => {
+
+    cy.get('[data-cy="isim-input"]').type('Emre')
+    cy.get('[data-cy="soyisim-input"]').type('Kaya')
+    cy.get('[data-cy="email-input"]').type('emrekaya@gmail.com')
+    cy.get('[data-cy="password-input"]').type('Test1234!')
+    cy.get('[data-cy="submit-button"]').click()
+
+    cy.get('[data-cy="response-message"]').should('be.visible')
+     })
+   })
+ })
+  
+ 
+ 
+   
